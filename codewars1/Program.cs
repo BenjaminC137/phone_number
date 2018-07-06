1eb23f7d1d34d714cfd942cd969efea4b7d283ff
benjaminc137 <benjamin.salasek@icloud.com>
2018-07-06 10:38:57 -0500

    test2

--- codewars1/Program.cs
+++ codewars1/Program.cs
@@ -9,9 +9,7 @@ namespace codewars1
             string phoneNumber = CreatePhoneNumber(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
 
             Console.WriteLine(phoneNumber);
-            Console.ReadLine();
-
-
+            Console.ReadKey();
 
         }
 
